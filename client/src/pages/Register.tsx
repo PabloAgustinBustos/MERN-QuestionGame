0795996f1006d5e5da4022b8d76f27dcf2ebdca3
Pablo Bustos <pablobustos609@gmail.com>
2023-01-30 20:09:22 -0300

    client | page de registro hecha. No tiene funcionalidad

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -4,9 +4,21 @@ import s from "./styles/Register.module.css"
 
 const Register = () => {
   return (
-    <div className={`bg-1`}>
-      <h1>Register</h1>
+    <div className={s.container}>
+      <h1 className={s.title}>Ingrese los datos para crear su cuenta de usuario</h1>
       
+      <form className={s.form}>
+        <input type="text" placeholder='Nikname'/>
+        <input type="email" placeholder='Email'/>
+        <input type="password" placeholder='Contraseña'/>
+        <input type="password" placeholder='Confirmar contraseña'/>
+
+        <button>Registrar</button>
+      </form>
+
+      <div className={`subText ${s.sub}`}>
+        ¿Ya está registrado? <Link to="/login">Ingrese sesión</Link>
+      </div>
     </div>
   )
 }
