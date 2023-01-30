16dd2eac7e3c54d0dbbbf75e74f20a30d4795d17
Pablo Bustos <pablobustos609@gmail.com>
2023-01-30 19:23:58 -0300

    seteados los fondos gradientes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,40 @@
 import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation, useParams } from 'react-router-dom'
 import s from "./App.module.css"
+import Categories from './pages/Categories'
+import Game from './pages/Game'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Score from './pages/Score'
+import SetPhoto from './pages/SetPhoto'
 
 function App() {
+  const location = useLocation()
+
+  const setBackground = () => {
+    if(location.pathname === "/"){
+      return "bg-categories"
+    }else if(location.pathname === "/game"){
+      return "bg-game"
+    }else if(location.pathname === "/score"){
+      return "bg-score"
+    }
+
+    return "bg-form"
+  }
+
   return (
-    <div className={s.app}>
+    <div className={`
+      ${s.app}
+      ${setBackground()}
+    `}>
       <Routes>
-        <Route path='/' element={<>Buenas</>}/>
-        <Route path='/2' element={<>Que tal</>}/>
+        <Route path='/' element={<Categories/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path="/register/set-photo" element={<SetPhoto/>}/>
+        <Route path="/game" element={<Game/>}/>
+        <Route path="/score" element={<Score/>}/>
       </Routes>
     </div>
   )
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import s from "./styles/Register.module.css"
 
 const Register = () => {
   return (
-    <div>
+    <div className={`bg-1`}>
       <h1>Register</h1>
+      
     </div>
   )
 }
